4f197b540162c66c05ead1031484c94282f388e8
havoriskova <ha.voriskova@gmail.com>
2023-08-10 23:18:02 +0200

    added class GeometricShape with polymorphism (each class has its own method with the same name GetResult())

--- AreaCalculatorApp/MainWindow.xaml.cs
+++ AreaCalculatorApp/MainWindow.xaml.cs
@@ -15,9 +15,13 @@ using System.Windows.Shapes;
 
 namespace AreaCalculatorApp
 {
-    /// <summary>
+    /// <summary>s
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //enum typeOfShape
+    //{
+    //    circle, rectangle, square, trapezoid, triangle
+    //}
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -37,7 +41,13 @@ namespace AreaCalculatorApp
                 Uri resourceUri = new Uri("/static/circle.png", UriKind.Relative);
                 imageOfShape.Source = new BitmapImage(resourceUri);
 
-            //    r
+                //    r
+                aStackPanel.Visibility = Visibility.Collapsed;
+                bStackPanel.Visibility = Visibility.Collapsed;
+                cStackPanel.Visibility = Visibility.Collapsed;
+                rStackPanel.Visibility = Visibility.Visible;
+                vStackPanel.Visibility = Visibility.Collapsed;
+
 
             }
             else if (shape == "rectangle")
@@ -46,6 +56,11 @@ namespace AreaCalculatorApp
                 imageOfShape.Source = new BitmapImage(resourceUri);
 
                 //     a, b
+                aStackPanel.Visibility = Visibility.Visible;
+                bStackPanel.Visibility = Visibility.Visible;
+                cStackPanel.Visibility = Visibility.Collapsed;
+                rStackPanel.Visibility = Visibility.Collapsed;
+                vStackPanel.Visibility = Visibility.Collapsed;
             }
             else if (shape == "square")
             {
@@ -53,46 +68,123 @@ namespace AreaCalculatorApp
                 imageOfShape.Source = new BitmapImage(resourceUri);
 
                 //  a
+                aStackPanel.Visibility = Visibility.Visible;
+                bStackPanel.Visibility = Visibility.Collapsed;
+                cStackPanel.Visibility = Visibility.Collapsed;
+                rStackPanel.Visibility = Visibility.Collapsed;
+                vStackPanel.Visibility = Visibility.Collapsed;
             }
             else if (shape == "trapezoid")
             {
                 Uri resourceUri = new Uri("/static/trapezoid.png", UriKind.Relative);
                 imageOfShape.Source = new BitmapImage(resourceUri);
 
-
                 //    a, c, v
+                aStackPanel.Visibility = Visibility.Visible;
+                bStackPanel.Visibility = Visibility.Collapsed;
+                cStackPanel.Visibility = Visibility.Visible;
+                rStackPanel.Visibility = Visibility.Collapsed;
+                vStackPanel.Visibility = Visibility.Visible;
             }
             else if (shape == "triangle")
             {
                 Uri resourceUri = new Uri("/static/triangle.png", UriKind.Relative);
                 imageOfShape.Source = new BitmapImage(resourceUri);
 
-
                 //   a, v
+                aStackPanel.Visibility = Visibility.Visible;
+                bStackPanel.Visibility = Visibility.Collapsed;
+                cStackPanel.Visibility = Visibility.Collapsed;
+                rStackPanel.Visibility = Visibility.Collapsed;
+                vStackPanel.Visibility = Visibility.Visible;
             }
             else
                 MessageBox.Show("sorry, there's an error");
 
-
-
-
+            vysledekTextBlock.Text = "0";
+            aTextBox.Text = "";
+            bTextBox.Text = "";
+            cTextBox.Text = "";
+            vTextBox.Text = "";
+            rTextBox.Text = "";   
         }
 
         private void vypocitatButton_Click(object sender, RoutedEventArgs e)
         {
 
-            //get the information from form and set other variables:
-            //
-            //Binding aBinding = new Binding("Text");
-            //aBinding.Source = aTextBox;
-            //vysledekTextBlock.SetBinding(TextBlock.TextProperty, aBinding);
+            //get the information from form and set other variables using switch - case
+            //save numbers from user inputs using binding ? or just .Text
+            //put the information into the right class method, which will return the result
+
+            // get name of the selected shape:
+            ComboBoxItem ComboItem = (ComboBoxItem)shapeComboBox.SelectedItem;
+            string shape = ComboItem.Name;
+
+            //setting type of 'result'
+            double vysledek = 0;
+
+            switch (shape)
+            {
+                case "circle":
+                    //Binding rCircle = new Binding("Text");
+                    //rCircle.Source = rTextBox;
+                    double rCircle = int.Parse(rTextBox.Text);
+                    Circle myCircle = new Circle(rCircle);
+                    vysledek = myCircle.GetResult();
+                    //
+                    break;
+                case "rectangle":
+                    //Binding aRectangle = new Binding("Text");
+                    //aRectangle.Source = aTextBox;
+                    //Binding bRectangle = new Binding("Text");
+                    //bRectangle.Source = bTextBox;
+                    double aRectangle = int.Parse(aTextBox.Text);
+                    double bRectangle = int.Parse(bTextBox.Text);
+                    Rectangle myRectangle = new Rectangle(aRectangle, bRectangle);
+                    vysledek = myRectangle.GetResult();
+
+                    break;
+                case "square":
+                    //Binding aSquare = new Binding("Text");
+                    //aSquare.Source = aTextBox;
+
+                    double aSquare = int.Parse(aTextBox.Text);
+                    Square mySquare = new Square(aSquare);
+                    vysledek = mySquare.GetResult();
+
+                    break;
+                case "trapezoid":
+                    //Binding aTrapezoid = new Binding("Text");
+                    //aTrapezoid.Source = aTextBox;
+                    //Binding cTrapezoid = new Binding("Text");
+                    //cTrapezoid.Source = cTextBox;
+                    //Binding vTrapezoid = new Binding("Text");
+                    //vTrapezoid.Source = vTextBox;
+                    double aTrapezoid = int.Parse(aTextBox.Text);
+                    double cTrapezoid = int.Parse(cTextBox.Text);
+                    double vTrapezoid = int.Parse(vTextBox.Text);
+                    Trapezoid myTrapezoid = new Trapezoid(aTrapezoid, cTrapezoid, vTrapezoid);
+                    vysledek = myTrapezoid.GetResult();
 
 
-            double vysledek = 5;
+                    break;
+                case "triangle":
+                    //Binding aTriangle = new Binding("Text");
+                    //aTriangle.Source = aTextBox;
+                    //Binding vTriangle = new Binding("Text");
+                    //vTriangle.Source = vTextBox;
+                    double aTriangle = int.Parse(aTextBox.Text);
+                    double vTriangle = int.Parse(vTextBox.Text);
+                    Rectangle myTriangle = new Rectangle(aTriangle, vTriangle);
+                    vysledek = myTriangle.GetResult();
 
 
-            //put the information into the right class method, which will return the result:
-
+                    break;
+                default:
+                    MessageBox.Show("sorry, there's an error");
+                    break;
+            }
+           
 
             // put the result into the TextBlock:
             vysledekTextBlock.Text = vysledek.ToString();
---
+++ AreaCalculatorApp/geometricShape.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorApp
+{
+    public abstract class GeometricShape
+    {
+        public virtual double GetResult()
+        {
+            return 0;
+        }
+    };
+
+    public class Circle : GeometricShape
+    {
+        public Circle(double rnum)
+        {
+            Rnum = rnum;
+        }
+        
+        public double Rnum { get; }
+            public override double GetResult()
+            {
+                return Math.Round(Math.PI * Math.Pow(Rnum, 2));
+
+            }
+       
+    }
+
+    public class Rectangle : GeometricShape
+    {
+          public Rectangle(double aNum, double bNum)
+            {
+                Anum = aNum;
+                Bnum = bNum;
+            }
+
+        public double Anum { get; }
+        public double Bnum { get; }
+        
+        public override double GetResult()
+        {
+            return Math.Round(Anum * Bnum);
+        }
+    }
+
+
+    public class Triangle : GeometricShape
+    {
+        public Triangle(double aNum, double vNum)
+        {
+            Anum = aNum;
+            Vnum = vNum;
+        }
+
+        public double Anum { get; }
+        public double Vnum { get; }
+
+        public override double GetResult()
+        {
+            return Math.Round((Vnum * Anum) / 2);
+        }
+    }
+
+
+    public class Square : GeometricShape
+    {
+        public Square(double aNum)
+        {
+            Anum = aNum;
+        }
+
+        public double Anum { get; }
+
+        public override double GetResult()
+        {
+            return Math.Round(Anum * Anum);
+        }
+    }
+
+    public class Trapezoid : GeometricShape
+    {
+        public Trapezoid(double aNum, double cNum, double vNum)
+        {
+            Anum = aNum;
+            Cnum = cNum;
+            Vnum = vNum;
+        }
+
+        public double Anum { get; }
+        public double Cnum { get; }
+        public double Vnum { get; }
+
+        public override double GetResult()
+        {
+            return Math.Round(((Anum + Cnum) * Vnum) / 2);
+        }
+    }
+
+
+}
